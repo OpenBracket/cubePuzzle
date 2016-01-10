b87052eeae259c6a63b941cadc0d807a2509b114
Jellyman305 <robpetrie4@icloud.com>
2016-01-10 20:05:29 +0000

    upload

---
+++ desktop/src/to/uk/openbracket/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package to.uk.openbracket.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import to.uk.openbracket.CubePuzzle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CubePuzzle(), config);
+	}
+}
